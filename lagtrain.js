ce647332418f6040a8875353bb2bcef5d4e1c211
Tiqur <Tiqur7@gmail.com>
2021-03-31 04:23:56 -0700

    Added download video function

--- lagtrain.js
+++ lagtrain.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('lagtrain')
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+// Download Lagtrain video from youtube
+const downloadLagtrainVideo = () => {
+  return new Promise(resolve => {
+    ytdl('https://www.youtube.com/watch?v=UnIhRpIT7nc&ab_channel=%E7%A8%B2%E8%91%89%E6%9B%87')
+      .pipe(fs.createWriteStream('lagtrain.mp4'))
+      .on('close', () => {
+          resolve();
+      })
+  })
+}
+
+
+downloadLagtrainVideo();
